942605982f782f367f08cb07979e50b9dc62aaac
Rafaelm <isaizanami@gmail.com>
2024-10-21 11:13:28 -0300

    criado model do usuario

--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,52 @@
 import Sequelize, { Model } from "sequelize";
+import bcryptjs from "bcryptjs";
 
 export default class User extends Model {
   static init(sequelize) {
     super.init(
       {
-        nome: Sequelize.STRING,
-        email: Sequelize.STRING,
-        password_hash: Sequelize.STRING,
-        password: Sequelize.VIRTUAL,
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: "campo tem que ter entre 3 e 255 caracteres.",
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+          unique: {
+            msg: "Email ja existe",
+          },
+          validate: {
+            isEmail: {
+              msg: "Email invalido.",
+            },
+          },
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+        },
+        password: {
+          type: Sequelize.VIRTUAL,
+          defaultValue: "",
+          validate: {
+            len: {
+              args: [6, 50],
+              msg: "Senha precisa ter entre 6 e 50 caracteres.",
+            },
+          },
+        },
       },
       { sequelize }
     );
+    this.addHook("beforeSave", async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
     return this;
   }
 }
