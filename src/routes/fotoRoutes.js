84b67afe88a5f825360fa5438960f559d6017de5
Rafaelm <isaizanami@gmail.com>
2024-10-23 18:44:56 -0300

    criado o Fotocontrollers e criado sua rota.

---
+++ src/routes/fotoRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import fotoController from "../controllers/Fotocontrollers";
+
+const router = new Router();
+
+router.post("/", fotoController.store);
+
+export default router;
