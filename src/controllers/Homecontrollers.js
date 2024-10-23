a08f3232e7294ab2352bce3a39b7045def3d68b4
Rafaelm <isaizanami@gmail.com>
2024-10-23 12:03:45 -0300

    criado o arquivo Alunocontrollers.js

--- src/controllers/Homecontrollers.js
+++ src/controllers/Homecontrollers.js
@@ -1,16 +1,6 @@
-import Aluno from "../models/Aluno";
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: "raiza",
-      sobrenome: "maiara",
-      email: "raiza@gmail.com",
-      idade: 32,
-      peso: 85,
-      altura: 1.7,
-    });
-    res.json(novoAluno);
+    res.json("index");
   }
 }
 
