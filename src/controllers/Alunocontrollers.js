890149d30a7a8cc8885e0f770a4ef20274b2188a
Rafaelm <isaizanami@gmail.com>
2024-10-24 23:20:14 -0300

    index  e show do Alunocontrollers editado.

--- src/controllers/Alunocontrollers.js
+++ src/controllers/Alunocontrollers.js
@@ -1,8 +1,27 @@
 import Aluno from "../models/Aluno";
+import Foto from "../models/Foto";
 
 class AlunoController {
   async index(req, res) {
-    const alunos = await Aluno.findAll();
+    const alunos = await Aluno.findAll({
+      attributes: [
+        "id",
+        "nome",
+        "sobrenome",
+        "email",
+        "idade",
+        "peso",
+        "altura",
+      ],
+      order: [
+        ["id", "DESC"],
+        [Foto, "id", "DESC"],
+      ],
+      include: {
+        model: Foto,
+        attributes: ["id", "url", "filename"],
+      },
+    });
     res.json(alunos);
   }
   async show(req, res) {
@@ -13,7 +32,25 @@ class AlunoController {
           errors: ["faltando Id"],
         });
       }
-      const aluno = await Aluno.findByPk(id);
+      const aluno = await Aluno.findByPk(id, {
+        attributes: [
+          "id",
+          "nome",
+          "sobrenome",
+          "email",
+          "idade",
+          "peso",
+          "altura",
+        ],
+        order: [
+          ["id", "DESC"],
+          [Foto, "id", "DESC"],
+        ],
+        include: {
+          model: Foto,
+          attributes: ["id", "url", "filename"],
+        },
+      });
 
       if (!aluno) {
         return res.status(400).json({
